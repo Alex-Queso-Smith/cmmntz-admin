a89ab1301819ee9ffe9499bddad1a63ec8d82429
Jesse <jesse@Jesses-MBP.fios-router.home>
2018-11-27 10:51:25 -0500

    app configs

--- config/application.rb
+++ config/application.rb
@@ -11,9 +11,27 @@ module ClassifilterAdmin
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
 
+    config.generators do |g|
+      g.orm :active_record, primary_key_type: :uuid
+    end
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+
+    # use sidekiq as background processor
+    config.active_job.queue_adapter = :sidekiq
+
+    # use the default queue for mailers (for now)
+    config.action_mailer.deliver_later_queue_name = 'default'
+
+    # add image sub dirs to pipline
+    Dir.glob("#{Rails.root}/app/assets/images/**/").each do |path|
+      config.assets.paths << path
+    end
+
+    config.generators do |g|
+      g.template_engine :haml
+    end
   end
 end
