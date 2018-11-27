22efd9b2eb5438140dca8e767cc925d5e8697a10
Jesse <jesse@Jesses-MBP.fios-router.home>
2018-11-27 10:53:25 -0500

    remove that which is not needed

--- app/models/author.rb
+++
@@ -1,7 +0,0 @@
-class Author < ApplicationRecord
-  include Fae::BaseModelConcern
-  def fae_display_field
-    name
-  end
-
-end
--- app/models/concerns/comment_base.rb
+++
@@ -1,8 +0,0 @@
-module CommentBase
-  extend ActiveSupport::Concern
-
-  def render_anonymously?
-    anonymous? || user.nil?
-  end
-
-end
