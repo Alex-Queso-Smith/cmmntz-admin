648bf822dadcb0d438b3a62d66ef47297a94bd06
Jesse <jesse@Jesses-MBP.fios-router.home>
2018-11-29 09:33:03 -0500

    stash

--- app/models/gallery.rb
+++ app/models/gallery.rb
@@ -2,6 +2,7 @@ class Gallery < ApplicationRecord
   has_many :arts
 
   validates :name, presence: true
+  validates :default_art_thread_expiration_days, numericality: {greater_than_or_equal_to: 0}, if: Proc.new {|g| !g.default_art_thread_expiration_days.blank? }
 
   def checker_settings
     {
