551ff452b290e2dcf13999810cf286cff7ba801a
Jesse <jesse@Jesses-MBP.fios-router.home>
2018-11-30 11:59:29 -0500

    add index, sort by created at

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_30_154225) do
+ActiveRecord::Schema.define(version: 2018_11_30_165750) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
@@ -75,6 +75,7 @@ ActiveRecord::Schema.define(version: 2018_11_30_154225) do
     t.boolean "disabled"
     t.boolean "deactivated"
     t.datetime "published_at"
+    t.index ["created_at"], name: "index_arts_on_created_at"
     t.index ["gallery_id"], name: "index_arts_on_gallery_id"
     t.index ["last_interaction_at"], name: "index_arts_on_last_interaction_at"
     t.index ["url"], name: "index_arts_on_url"
