c459ad5633afa189499c248bac74ca8eb9fbc089
Jesse <jesse@Jesses-MBP.fios-router.home>
2018-12-04 14:09:54 -0500

    order

--- app/controllers/api/v1/arts_controller.rb
+++ app/controllers/api/v1/arts_controller.rb
@@ -3,7 +3,7 @@ class Api::V1::ArtsController < ApiController
   def show
     @art = Art.find(params[:id])
     display_mode = params[:display_mode] || ""
-    @comments = @art.comments_for_display_mode(display_mode)
+    @comments = @art.comments_for_display_mode(display_mode).order(created_at: :desc)
   end
 
 end
