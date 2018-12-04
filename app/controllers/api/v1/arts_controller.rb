463b13af8a5df4dff251056ab8adee55ef7fa7a8
Jesse <jesse@Jesses-MBP.fios-router.home>
2018-12-04 13:26:25 -0500

    spacing

--- app/controllers/api/v1/arts_controller.rb
+++ app/controllers/api/v1/arts_controller.rb
@@ -1,9 +1,9 @@
 class Api::V1::ArtsController < ApiController
 
-def show
-  @art = Art.find(params[:id])
-  display_mode = params[:display_mode] || ""
-  @comments = @art.comment_for_display_mode(display_mode)
-end
+  def show
+    @art = Art.find(params[:id])
+    display_mode = params[:display_mode] || ""
+    @comments = @art.comment_for_display_mode(display_mode)
+  end
 
 end
