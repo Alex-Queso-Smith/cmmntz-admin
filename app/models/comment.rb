3d1360c68d554d585c20c068eaf26cb6ac17abb8
Jesse <jesse@Jesses-MBP.fios-router.home>
2018-12-04 15:38:20 -0500

    space

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -8,7 +8,7 @@ class Comment < ApplicationRecord
   scope :not_deleted, -> {where(deleted: false)}
   scope :deleted, -> {where(deleted: true)}
 
-  scope:for_non_blocked_users, -> {
+  scope :for_non_blocked_users, -> {
     joins(:art)
     .joins("left join gallery_blacklistings on gallery_blacklistings.user_id = comments.user_id AND gallery_blacklistings.gallery_id = arts.gallery_id")
     .where("gallery_blacklistings.id IS NULL")
