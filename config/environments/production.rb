588d1f39b25f744bada086d541fa76c2967ad828
Jesse <jesse@Jesses-MBP.fios-router.home>
2018-12-06 13:36:03 -0500

    use same setting as other apps

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -23,13 +23,18 @@ Rails.application.configure do
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
   config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
+  if ENV["RAILS_LOG_TO_STDOUT"].present?
+    logger           = ActiveSupport::Logger.new(STDOUT)
+    logger.formatter = config.log_formatter
+    config.logger = ActiveSupport::TaggedLogging.new(logger)
+  end
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
@@ -63,7 +68,7 @@ Rails.application.configure do
 
   # Use a real queuing backend for Active Job (and separate queues per environment)
   # config.active_job.queue_adapter     = :resque
-  # config.active_job.queue_name_prefix = "classifilter-admin_#{Rails.env}"
+  # config.active_job.queue_name_prefix = "classifilter_master_#{Rails.env}"
 
   config.action_mailer.perform_caching = false
 
