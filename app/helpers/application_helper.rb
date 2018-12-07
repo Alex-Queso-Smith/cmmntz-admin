d5d2a81f2c1393bca5e9ed91a21fc201a73634f2
Jesse <jesse@Jesses-MBP.fios-router.home>
2018-12-07 09:57:30 -0500

    add gal name to nav bar when logged in

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,11 @@
 module ApplicationHelper
-
+  def nav_title(current_gallery)
+    base = "ClassiAdmin"
+    if current_gallery
+      base += ": #{current_gallery.name}"
+    end
+    base
+  end
   def display_date timestamp, time = true
     return "" if timestamp.blank?
     timestamp.strftime(time ? "%Y-%m-%d %H:%M" : "%Y-%m-%d")
