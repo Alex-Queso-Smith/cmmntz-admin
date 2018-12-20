0492e988bf902e85599dfb3c988527402248ca7a
Jesse <jesse@Jesses-MBP.fios-router.home>
2018-12-20 11:18:10 -0500

    moving stuff part 2

--- app/javascript/react/containers/members/MembersContainer.js
+++ app/javascript/react/containers/members/MembersContainer.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-import MemberTile from '../../components/MemberTile';
+import MemberTile from '../../components/members/MemberTile';
 
 class MembersContainer extends React.Component {
   state = {
