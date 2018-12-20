862152dbe49a515e0e6c32f0853ca77294a4b1c9
Jesse <jesse@Jesses-MBP.fios-router.home>
2018-12-20 13:56:28 -0500

    change link

--- app/helpers/arts_helper.rb
+++ app/helpers/arts_helper.rb
@@ -2,10 +2,16 @@ module ArtsHelper
   def gallery_artist_name(art)
     return "" unless art.gallery_artist
     if art.gallery_artist.customer
-      art.gallery_artist.customer.name
+      m = "#{art.gallery_artist.customer.name} as #{art.gallery_artist.artist_name} #{gallery_artist_assign_link(art, "Reassign")}"
     else
       assign = link_to "Assign to member", edit_gallery_artist_path(art.gallery_artist)
-      "#{art.gallery_artist.artist_name}: #{assign}".html_safe
+      m = "#{art.gallery_artist.artist_name}: #{gallery_artist_assign_link(art)}"
     end
+
+    m.html_safe
+  end
+
+  def gallery_artist_assign_link(art, title = "Assign to member")
+    link_to title, edit_gallery_artist_path(art.gallery_artist)
   end
 end
