d9382d1da07fcd0619f82ef780df0cbec4b323bb
Jesse <jesse@Jesses-MBP.fios-router.home>
2018-12-20 15:03:35 -0500

    add is mod and is admin to comments return for art

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -6,6 +6,8 @@ class Customer < ApplicationRecord
 
   validates :first_name, :last_name, :role, presence: true
 
+  scope :account_for_gallery_and_user, -> (gallery_id, user_id) { where(gallery_id: gallery_id, user_account_id: user_id) }
+
   acts_as_authentic do |c|
     c.login_field = :email
     c.validate_email_field = false
