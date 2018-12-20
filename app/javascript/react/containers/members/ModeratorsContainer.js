61ee67fbd6bbdffe5125cbb3a789d89b0febc3cb
Jesse <jesse@Jesses-MBP.fios-router.home>
2018-12-20 11:29:26 -0500

    remove table upper border

---
+++ app/javascript/react/containers/members/ModeratorsContainer.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import { Input } from '../../components/FormComponents';
+
+class ModeratorsContainer extends React.Component {
+  state = {}
+}
+
+export default ModeratorsContainer;
