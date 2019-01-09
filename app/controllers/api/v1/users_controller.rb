2b9afb0ebd85c0c8fe39f98f749ff7062fa95b2c
Jesse <jesse@Jesses-MBP.fios-router.home>
2019-01-09 14:57:16 -0500

    check in

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -2,6 +2,9 @@ class Api::V1::UsersController < ApiController
   load_and_authorize_resource
 
   def index
-    @users = @users
+    @users = @users.not_guest.order(created_at: :desc)
+    if params[:search]
+      @users = @users.sort_order(params[:search])
+    end
   end
 end
