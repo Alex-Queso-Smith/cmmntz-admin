19df7d04230cbea93dd78a7ec6a8ab0a73f0467e
Jesse <jesse@Jesses-MBP.fios-router.home>
2019-01-28 11:56:30 -0500

    set up controller

---
+++ app/controllers/api/v1/signups_controller.rb
@@ -0,0 +1,19 @@
+class Api::V1::SignupsController < ApiController
+  skip_before_action *ALL_FILTERS, only: [:create]
+  before_action :require_no_customer, only: [:create]
+
+  def create
+    @signup = Signup.new(signup_params)
+    if @signup.valid?
+      render json: { message: "Gallery created!" }
+    else
+      render json: @signup.errors, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def signup_params
+    params.require(:signup).permit(:gallery_name, :customer_first_name, :customer_last_name, :customer_password, :customer_password_confirmation, :customer_email)
+  end
+end
