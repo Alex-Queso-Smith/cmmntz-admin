c8c4972de9584288a4e8fd1fef861a4c71a788f4
Jesse <jesse@Jesses-MacBook-Pro.local>
2019-01-29 13:43:16 -0500

    pare functions

--- app/javascript/react/containers/settings/ModerationSettingsContainer.js
+++ app/javascript/react/containers/settings/ModerationSettingsContainer.js
@@ -14,12 +14,8 @@ class ModerationSettingsContainer extends React.Component {
     notifyOnCommentApprovalNeeded: false
   }
 
-  handleFilterByClick = this.handleFilterByClick.bind(this);
-  handleFilterClick = this.handleFilterClick.bind(this);
-  handleSortDirClick = this.handleSortDirClick.bind(this);
   handleChange = this.handleChange.bind(this);
   handleSubmit = this.handleSubmit.bind(this);
-  handleSortOptCheckChange = this.handleSortOptCheckChange.bind(this);
 
   componentDidMount(){
     FetchDidMount(this, `/api/v1/galleries/${document.getElementById('ca-app').getAttribute('data-gallery-id')}.json`)
@@ -45,16 +41,6 @@ class ModerationSettingsContainer extends React.Component {
     .catch(error => console.error(`Error in fetch: ${error.message}`));
   }
 
-  handleSortOptCheckChange(event) {
-    var target = event.target
-    var newOpts = this.state.sortOpts
-    newOpts[target.name] = target.checked
-
-    this.setState({
-      sortOpts: newOpts
-    })
-  }
-
   handleChange(event){
     const target = event.target;
     const name = target.name;
@@ -77,70 +63,6 @@ class ModerationSettingsContainer extends React.Component {
       this.setState({ sortOpts: opts })
     }
   };
-
-  handleFilterByClick(event){
-    const target = event.target;
-    const name = target.name;
-    const value = target.value;
-
-    var opts = this.state.sortOpts
-    opts[name] = value;
-
-    this.setState({ sortOpts: opts })
-  }
-
-  handleFilterClick(event){
-    event.preventDefault();
-    const target = event.target;
-    const name = target.getAttribute('data-value');
-    var opts = this.state.sortOpts
-
-    const right = [
-      "dislike_percent",
-      "dislike_a_lot_percent",
-      "trash_percent",
-      "warn_percent",
-      "sad_percent",
-      "boring_percent",
-      "angry_percent"
-    ]
-
-    if (right.includes(name)) {
-      if (opts.notFilterList.includes(name)) {
-        var newFilters = opts.notFilterList.filter(v => v != name)
-        opts.notFilterList = newFilters
-        opts.filterList.push(name)
-      } else if (opts.filterList.includes(name)) {
-        var newFilters = opts.filterList.filter(v => v != name)
-        opts.filterList = newFilters
-      } else {
-        opts.notFilterList.push(name)
-      }
-    } else {
-      if (opts.filterList.includes(name)){
-        var newFilters = opts.filterList.filter(v => v != name)
-        opts.filterList = newFilters
-        opts.notFilterList.push(name)
-      } else if (opts.notFilterList.includes(name)) {
-        var newFilters = opts.notFilterList.filter(v => v != name)
-        opts.notFilterList = newFilters
-      } else {
-        opts.filterList.push(name)
-      }
-    }
-
-    this.setState({ sortOpts: opts })
-  }
-
-  handleSortDirClick(event){
-    event.preventDefault();
-    var value = (this.state.sortOpts.sortDir == "asc") ? "desc" : "asc"
-
-    var opts = this.state.sortOpts
-    opts.sortDir = value
-
-    this.setState({ sortOpts: opts })
-  }
 
   handleSubmit(event){
     event.preventDefault();
