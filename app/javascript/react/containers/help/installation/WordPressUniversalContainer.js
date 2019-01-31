770b13df8219f95957e84ef3728497eaea350322
Alex Smith <alex@classibridge.com>
2019-01-31 11:14:04 -0500

    wordpress instructions updating

--- app/javascript/react/containers/help/installation/WordPressUniversalContainer.js
+++ app/javascript/react/containers/help/installation/WordPressUniversalContainer.js
@@ -25,12 +25,16 @@ class WordPressUniversalContainer extends React.Component {
       marginLeft: "20px"
     }
 
+    var boldStyle = {
+
+    }
+
     return(
       <div className="cf-settings-base container">
-        <h2 className="text-center">Classifilter Widget Universal Installation Instructions</h2>
+        <h2 className="text-center">Classifilter Widget WordPress Installation Instructions</h2>
 
         <br />
-        <p className="text-center">Classifilter can be installed on most websites via the universal JavaScript installation code below.</p>
+        <p className="text-center">Classifilter can be installed on most websites via the WordPress JavaScript installation code below.</p>
         <p className="text-center">Please note that these instructions do require some technical knowledge and are intended for use by developers.</p>
         <hr />
 
@@ -38,34 +42,14 @@ class WordPressUniversalContainer extends React.Component {
         <h3>Getting Started</h3>
         <ul>
           <li>{galleryInstructionDisplay}</li>
-          <li>You will need access to the HTML of the pages that you intend to add Classifilter to.</li>
+          <br />
+          <li>You will need access to the HTML of the pages that you intend to add Classifilter to. Please follow the instructions below to find the HTML and insert the appropriate code.</li>
         </ul>
 
         <br />
         <h3>Script</h3>
-        <p style={indentStyle}>This is the script that you will need. You can paste this into your theme's comment.php file.</p>
 
-
-        <pre>
-          &lt;script src="https://api.classifilter.com/javascripts/classifilter-embed.js"&gt;&lt;/script&gt;
-          <br/>
-          &lt;script type="text/javascript"&gt;
-          <br/>
-          Classifilter.init({`{`}<br/>
-          {galleryIdDisplay},<br/>
-          topics: '{`<`}?php foreach((get_the_category()) as $category) {`{`} echo $category->cat_name . ','; {`}`} ?{`>`}',<br/>
-          publishDate: '{`<`}?php the_date('Y-m-d h:i:s'); ?{`>`}',<br/>
-          artistName: '{`<`}?php the_author(); ?{`>`}',<br/>
-          type: "article"<br/>,
-          urlOverride: '{`<`}?php echo get_permalink($post); ?{`>`}'<br/>,
-          {`}`});<br/>
-          &lt;/script&gt;<br/>
-          &lt;noscript&gt;<br/>
-          Please enable JavaScript to see comments powered by Classifilter.<br/>
-          &lt;/noscript&gt;
-        </pre>
-
-        <p style={indentStyle}>You can find your comments.php file via the following steps</p>
+        <p style={indentStyle}>You will need to locate the comments.php file via the following steps.</p>
         <ol>
           <li>
             Access your Word Press File System.
@@ -85,64 +69,38 @@ class WordPressUniversalContainer extends React.Component {
             </ol>
           </li>
           <li>
-            make a backup of your original comments.php
+            Make a backup of your original comments.php.
           </li>
           <li>
-            open the comments.php file and replace it's content with the script
+            Open the comments.php file and replace it's entire contents with the script below.
           </li>
         </ol>
-        <br />
-        <h3>Configuration Variables</h3>
-        <p style={indentStyle}>The following configuration variables are required and must be passed into the script.</p>
-        <ul>
-          <li className="row">
-            <strong className="col-2">galleryId: </strong>
-            <div className="col-10">
-              This is your id that was assigned to your organization on sign up and should be auto-populated in the script above. If it is not, please go to `https://admin.classifilter.com/signup` and signup.
-            </div>
-          </li>
-          <br />
-          <li className="row">
-            <strong className="col-2">topics: </strong>
-            <div className="col-10">
-              A list of topics that relate to the content of your page. These should be passed in separated by commas.
-            </div>
-          </li>
-          <br />
-          <li className="row">
-            <strong className="col-2">publishDate: </strong>
-            <div className="col-10">
-              The date of publication. This should be in the format of YYYY-MM-DD
-            </div>
-          </li>
-          <br />
-          <li className="row">
-            <strong className="col-2">artistName: </strong>
-            <div className="col-10">
-              The name of the author of the article. Example: "Jane Smith"
-            </div>
-          </li>
-          <br />
-          <li className="row">
-            <strong className="col-2">type: </strong>
-            <div className="col-10">
-              The type of content on the page. This can be any value you choose.
-            </div>
-          </li>
-        </ul>
 
+        <h5 className="text-center">DO NOT EDIT OR CHANGE THIS SCRIPT</h5>
+        <pre>
+        &lt;script src="https://api.classifilter.com/javascripts/classifilter-embed.js"&gt;&lt;/script&gt;
+        <br/>
+        &lt;script type="text/javascript"&gt;
+        <br/>
+        Classifilter.init({`{`}<br/>
+          {galleryIdDisplay},<br/>
+          topics: '{`<`}?php foreach((get_the_category()) as $category) {`{`} echo $category->cat_name . ','; {`}`} ?{`>`}',<br/>
+          publishDate: '{`<`}?php the_date('Y-m-d h:i:s'); ?{`>`}',<br/>
+          artistName: '{`<`}?php the_author(); ?{`>`}',<br/>
+          type: "article"<br/>,
+          urlOverride: '{`<`}?php echo get_permalink($post); ?{`>`}'<br/>,
+          {`}`});<br/>
+          &lt;/script&gt;<br/>
+          &lt;noscript&gt;<br/>
+          Please enable JavaScript to see comments powered by Classifilter.<br/>
+          &lt;/noscript&gt;
+          </pre>
         <br />
-        <h3>Optional Variables</h3>
-        <p style={indentStyle}>These are variable that can be added to the configuration section of the script.</p>
-        <ul>
-          <li className="row">
-            <strong className="col-2">urlOverride:</strong>
-            <div className="col-10">
-              The classifilter widget automatically creates a thread for the url of a page that it is added to. However it will also allow you to override the url of the page and use a different url instead. This will allow you to share the same thread on the same article on different versions of your site or if you have multiple url formats for your content.
-            </div>
-          </li>
-        </ul>
 
+        <h3>After Installation</h3>
+          <p style={indentStyle}>
+            Adjust the settings for the widget via the navbar above to customize the experience for your users as well as set default settings for the widget on your site. Please visit the FAQ page for any additional help.
+          </p>
       </div>
     )
   }
