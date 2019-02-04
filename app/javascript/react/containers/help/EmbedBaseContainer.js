dfbbe018ec1ba1eb51c49b0c480e042875088100
Jesse <jesse@Jesses-MBP.fios-router.home>
2019-02-04 11:48:20 -0500

    coming soon overlay

--- app/javascript/react/containers/help/EmbedBaseContainer.js
+++ app/javascript/react/containers/help/EmbedBaseContainer.js
@@ -21,58 +21,116 @@ class HelpBaseContainer extends React.Component {
           <div onClick={ () => { this.props.history.push('/help/embed/wordpress') } } style={pointerReady} className="col-3 cf-install-tile">
             <h3 style={overideStyle} className="text-center">Word Press</h3>
           </div>
-          <div className="col-3 cf-install-tile">
+          <div className="col-3 cf-install-tile coming-soon">
+            <div className="overlay"><h3>Coming Soon</h3></div>
             <h3 className="text-center">
-              Coming Soon
+              Blogger
             </h3>
           </div>
-          <div className="col-3 cf-install-tile">
+          <div className="col-3 cf-install-tile coming-soon">
+            <div className="overlay"><h3>Coming Soon</h3></div>
             <h3 className="text-center">
-              Coming Soon
-            </h3>
-          </div>
-        </div>
-        <div className="row justify-content-between">
-          <div className="col-3 cf-install-tile">
-            <h3 className="text-center">
-              Coming Soon
-            </h3>
-          </div>
-          <div className="col-3 cf-install-tile">
-            <h3 className="text-center">
-              Coming Soon
-            </h3>
-          </div>
-          <div className="col-3 cf-install-tile">
-            <h3 className="text-center">
-              Coming Soon
-            </h3>
-          </div>
-          <div className="col-3 cf-install-tile">
-            <h3 className="text-center">
-              Coming Soon
+              Tumblr
             </h3>
           </div>
         </div>
         <div className="row justify-content-between">
-          <div className="col-3 cf-install-tile">
+          <div className="col-3 cf-install-tile coming-soon">
+            <div className="overlay"><h3>Coming Soon</h3></div>
             <h3 className="text-center">
-              Coming Soon
+              Squarespace
             </h3>
           </div>
-          <div className="col-3 cf-install-tile">
+          <div className="col-3 cf-install-tile coming-soon">
+            <div className="overlay"><h3>Coming Soon</h3></div>
             <h3 className="text-center">
-              Coming Soon
+              Typepad
             </h3>
           </div>
-          <div className="col-3 cf-install-tile">
+          <div className="col-3 cf-install-tile coming-soon">
+            <div className="overlay"><h3>Coming Soon</h3></div>
             <h3 className="text-center">
-              Coming Soon
+              Movable Type
             </h3>
           </div>
-          <div className="col-3 cf-install-tile">
+          <div className="col-3 cf-install-tile coming-soon">
+            <div className="overlay"><h3>Coming Soon</h3></div>
             <h3 className="text-center">
-              Coming Soon
+              Drupal
+            </h3>
+          </div>
+        </div>
+        <div className="row justify-content-between">
+          <div className="col-3 cf-install-tile coming-soon">
+            <div className="overlay"><h3>Coming Soon</h3></div>
+            <h3 className="text-center">
+              Joomla
+            </h3>
+          </div>
+          <div className="col-3 cf-install-tile coming-soon">
+            <div className="overlay"><h3>Coming Soon</h3></div>
+            <h3 className="text-center">
+              Wix
+            </h3>
+          </div>
+          <div className="col-3 cf-install-tile coming-soon">
+            <div className="overlay"><h3>Coming Soon</h3></div>
+            <h3 className="text-center">
+              Weebly
+            </h3>
+          </div>
+          <div className="col-3 cf-install-tile coming-soon">
+            <div className="overlay"><h3>Coming Soon</h3></div>
+            <h3 className="text-center">
+              Strikingly
+            </h3>
+          </div>
+          <div className="col-3 cf-install-tile coming-soon">
+            <div className="overlay"><h3>Coming Soon</h3></div>
+            <h3 className="text-center">
+              Jekyll
+            </h3>
+          </div>
+          <div className="col-3 cf-install-tile coming-soon">
+            <div className="overlay"><h3>Coming Soon</h3></div>
+            <h3 className="text-center">
+              Ghost
+            </h3>
+          </div>
+          <div className="col-3 cf-install-tile coming-soon">
+            <div className="overlay"><h3>Coming Soon</h3></div>
+            <h3 className="text-center">
+              HubSpot
+            </h3>
+          </div>
+          <div className="col-3 cf-install-tile coming-soon">
+            <div className="overlay"><h3>Coming Soon</h3></div>
+            <h3 className="text-center">
+              Shopify
+            </h3>
+          </div>
+          <div className="col-3 cf-install-tile coming-soon">
+            <div className="overlay"><h3>Coming Soon</h3></div>
+            <h3 className="text-center">
+              Adobe Muse
+            </h3>
+          </div>
+          <div className="col-3 cf-install-tile coming-soon">
+            <div className="overlay"><h3>Coming Soon</h3></div>
+            <h3 className="text-center">
+              Jimdo
+            </h3>
+          </div>
+          <div className="col-3 cf-install-tile coming-soon">
+            <div className="overlay"><h3>Coming Soon</h3></div>
+            <h3 className="text-center">
+              Postach.io
+            </h3>
+          </div>
+          <div className="col-3 cf-install-tile coming-soon">
+            <div className="overlay"><h3>Coming Soon</h3></div>
+            <h3 className="text-center">
+              Pattern
             </h3>
           </div>
         </div>
